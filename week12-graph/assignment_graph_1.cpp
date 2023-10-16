706992524a5e3d179726759c02413f1295bf5280
saqibshafin <shafinsaqib@gmail.com>
2023-10-16 20:01:56 +0600

    added week12 assignment_graph_1

---
+++ week12-graph/assignment_graph_1.cpp
@@ -0,0 +1,47 @@
+// Time complexity:
+// Space complexity:
+
+#include <iostream>
+#include <vector>
+
+int main() {}
+
+/*
+
+Assignment on Graph -1
+
+Write a function that does the following task.
+
+Given a graph with A nodes.
+The edges in the graph are given in a 2D array B.
+There is an undirected edge between B[i][0] and B[i][1].
+Find the number of connected components in the given graph.
+
+Mention the time and space complexity of your solution.
+
+
+Constraints:
+1 <= A <= 10^5
+1 <= |B| <= 10^5
+1 <= B[i][0], B[i][1] <= A
+
+
+Example 1:
+
+A = 4
+B =
+[[1, 2],
+[2, 3]]
+Output : 2
+Explanation: The two connected components are [1, 2, 3] and [4].
+
+Example 2:
+
+A = 3
+B =
+[[1, 2],
+[2, 1]]
+Output: 2
+Explanation: The two connected components are [1, 2] and [3].
+
+*/
